c3d8490bcc3ef7e7b2d31c84335197484a2e0885
Hewerton <hewertonlourenco@gmail.com>
2024-02-05 18:09:57 -0300

    refact(ui): implement the login card with card component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,35 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
-import { auth } from "@/server/auth";
+import { auth, authConfig } from "@/server/auth";
+import { redirect } from "next/navigation";
+import {
+ Card,
+ CardContent,
+ CardDescription,
+ CardFooter,
+ CardHeader,
+ CardTitle,
+} from "@/components/ui/card";
 import Signin from "@/components/signin";
 
 async function Page() {
+ const { providers } = authConfig;
  const session = await auth();
+
  if (!session)
   return (
    <main className=" flex justify-center items-center h-svh w-svw">
-    <Signin />
+    <Card className="w-[350px]">
+     <CardHeader className=" text-center">
+      <CardTitle>Entre com um dos serviços abaixo</CardTitle>
+      <CardDescription>Autentique-se com o Google ou Github.</CardDescription>
+     </CardHeader>
+     <CardContent>
+      <Signin />
+     </CardContent>
+    </Card>
    </main>
   );
- return (
-  <main className=" flex justify-center items-center h-svh w-svw">
-   {/* <Button variant={"default"}>Logar com o Google</Button> */}
-   JA ESTA LOGADO! {session.user?.name}
-  </main>
- );
+ return redirect("/dashboard");
 }
 
 export default Page;
