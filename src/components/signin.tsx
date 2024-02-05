200d2d00c1992fc490bdb6392a519bd99f2e8bc3
Hewerton <hewertonlourenco@gmail.com>
2024-02-05 18:09:10 -0300

    refact(ui): button of signin

--- src/components/signin.tsx
+++ src/components/signin.tsx
@@ -1,24 +1,33 @@
+"use client";
 import React from "react";
 import { Button } from "./ui/button";
-import { signIn } from "@/server/auth";
+import { authLogin } from "@/app/actions/authLogin";
+import { FaGoogle } from "react-icons/fa";
+import { VscGithub } from "react-icons/vsc";
 
-function Signin({
- provider,
- ...props
-}: {
- provider?: string;
-} & React.ComponentPropsWithRef<typeof Button>) {
- console.log(provider);
-
+function Signin() {
+ async function onClick(provider: "google" | "github") {
+  await authLogin(provider);
+ }
  return (
-  <form
-   action={async () => {
-    "use server";
-    await signIn(provider);
-   }}
-  >
-   <Button {...props}>Sign In - Google</Button>
-  </form>
+  <div className=" flex gap-4 justify-center">
+   <Button
+    className=" flex gap-4 justify-center w-full"
+    variant={"outline"}
+    size={"lg"}
+    onClick={() => onClick("google")}
+   >
+    <FaGoogle className="h-5 w-5" />
+   </Button>
+   <Button
+    className=" flex gap-4 justify-center w-full"
+    variant={"outline"}
+    size={"lg"}
+    onClick={() => onClick("github")}
+   >
+    <VscGithub className="h-5 w-5" />
+   </Button>
+  </div>
  );
 }
 
