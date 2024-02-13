63db4f3cab7fd7dc0d4e30d7c7097dec1a533c1f
Hewerton <hewertonlourenco@gmail.com>
2024-02-13 13:31:04 -0300

    chore(db): fixing the field names of the session table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -2,20 +2,21 @@ import { sql } from "drizzle-orm";
 import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
 
 export type databaseUser = typeof userTable._.inferSelect;
+export type databaseSession = typeof sessionTable._.inferSelect;
 
 export const userTable = sqliteTable("userTable", {
   id: text("id").notNull().primaryKey().unique(),
   email: text("email").notNull().unique(),
-  githubId: integer('githubId').unique(),
-  username: text('username'),
+  githubId: integer("githubId").unique(),
+  username: text("username"),
 });
 
 export const sessionTable = sqliteTable("sessionTable", {
   id: text("id").notNull().primaryKey(),
-  userId: integer("userId")
+  userId: text("user_id")
     .notNull()
     .references(() => userTable.id),
-  expiresAt: text("expiresAt")
+  expiresAt: text("expires_at")
     .default(sql`CURRENT_TIMESTAMP`)
     .notNull(),
 });
