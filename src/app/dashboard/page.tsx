ca2b5b084e9a8f78a2d6a4845ca376ed3cd558fb
Hewerton <hewertonlourenco@gmail.com>
2024-02-12 21:59:46 -0300

    chore(ui): dashboard page, a lot work to do

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+ ResizableHandle,
+ ResizablePanel,
+ ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
+function page() {
+ return (
+  // <main className=" h-svh w-svw">
+  <ResizablePanelGroup
+   direction="horizontal"
+   className="min-h-svh rounded-lg border"
+  >
+   <ResizablePanel defaultSize={20}>
+    <div className="flex h-full items-center justify-center p-6">
+     <span className="font-semibold">Sidebar</span>
+    </div>
+   </ResizablePanel>
+   <ResizableHandle withHandle />
+   <ResizablePanel defaultSize={80}>
+    <div className="flex h-full items-center justify-center p-6">
+     <span className="font-semibold">Content</span>
+    </div>
+   </ResizablePanel>
+  </ResizablePanelGroup>
+  // </main>
+ );
+}
+
+export default page;
