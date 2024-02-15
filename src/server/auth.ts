66d0a38874b7708903aac49a2335f5fcc227b774
Hewerton <hewertonlourenco@gmail.com>
2024-02-15 11:59:03 -0300

    refact(auth): add google client for Oauth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,11 +4,17 @@ import { createClient } from "@libsql/client";
 import { databaseUser } from "@/server/db/schema";
 import { cache } from "react";
 import { cookies } from "next/headers";
-import { GitHub } from "arctic";
+import { GitHub, Google } from "arctic";
 
 export const github = new GitHub(
   process.env.GITHUB_CLIENT_ID!,
   process.env.GITHUB_CLIENT_SECRET!,
+);
+
+export const google = new Google(
+  process.env.GOOGLE_CLIENT_ID!,
+  process.env.GOOGLE_CLIENT_SECRET!,
+  "http://localhost:3000/login/google/callback"
 );
 
 const client = createClient({
