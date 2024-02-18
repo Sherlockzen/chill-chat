79c2ea12ddec2a89193b7dc7ee6d08a601321f97
Hewerton <hewertonlourenco@gmail.com>
2024-02-18 20:10:45 -0300

    refact(auth): adding profile info to return in google authentication

--- src/app/login/google/route.ts
+++ src/app/login/google/route.ts
@@ -6,7 +6,9 @@ export async function GET(): Promise<Response> {
   const state = generateState();
   const codeVerifier = generateCodeVerifier();
 
-  const url = await google.createAuthorizationURL(state, codeVerifier);
+  const url = await google.createAuthorizationURL(state, codeVerifier, {
+    scopes: ["profile", "email"]
+  });
 
   cookies().set("google_oauth_state", state, {
     path: "/",
