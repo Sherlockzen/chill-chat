fe0d341506acba45ec634b44fa029ffbe2dd7ba2
Hewerton <hewertonlourenco@gmail.com>
2024-03-02 22:32:35 -0300

    refactor(ui): implementing a new design for the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,38 @@
 import "dotenv/config";
 import { validateRequest } from "@/server/auth";
+import { Button } from "@/components/ui/button";
+import { FcGoogle } from "react-icons/fc";
+import { GrGithub } from "react-icons/gr";
 
 export default async function Home() {
-  const { user } = await validateRequest();
+  // const { user } = await validateRequest();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="text-5xl">starting</div>
-      {user && <div>Usuario logado: {user.email}</div>}
+    <main className="flex h-svh flex-col items-center justify-between p-24">
+      <div className={" flex border border-black w-full h-full"}>
+        {/*left*/}
+        <div
+          className={" flex flex-col items-center justify-center w-full h-full"}
+        >
+          <div className={""}>
+            <h1 className={" font-bold text-4xl "}>Realize o Login</h1>
+            <div className={" font-normal text-xl text-[#71717A]"}>
+              Seja bem vindo! Escolha seu método preferido:
+            </div>
+            <div className={" w-full flex gap-5 mt-10"}>
+              <Button size={"lg"} className={"w-full uppercase"}>
+                <FcGoogle className={"mr-2 h-4 w-4"} />
+                Google
+              </Button>
+              <Button size={"lg"} className={"w-full uppercase"}>
+                <GrGithub className={"mr-2 h-full w-4"} />
+                Github
+              </Button>
+            </div>
+          </div>
+        </div>
+        {/*right*/}
+        <div className={" bg-[#6172F3] w-full h-full"}></div>
+      </div>
     </main>
   );
 }
